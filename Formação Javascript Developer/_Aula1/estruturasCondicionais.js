d9d35a3d016b9c43385a3532c7f5d307e7541157
CamilaLagoPy <camilalago.py@outlook.com>
2026-06-22 10:36:03 -0400

    estudos1

---
+++ Formação Javascript Developer/_Aula1/estruturasCondicionais.js
@@ -0,0 +1,13 @@
+/*São instruções para realizar determinadas
+tarefas a partir de uma condição, seja de decisão ou repetição*/
+
+const camilaECasada = true;
+const camilaETemFilhos = false;
+
+
+/* if = Se...*/
+if (camilaECasada && camilaETemFilhos) {
+    console.log("Camila é casada e tem filhos");
+} else {
+    console.log("Camila não é casada ou não tem filhos");
+}
